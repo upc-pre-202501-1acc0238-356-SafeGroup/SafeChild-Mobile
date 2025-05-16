21ae389e528734ed414739cb044ebe8208022fa0
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-16 16:21:27 -0500

    feat(Payments): add payment method view model as a parameter for navigator

--- app/src/main/java/com/example/safechild/MainActivity.kt
+++ app/src/main/java/com/example/safechild/MainActivity.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.safechild.ui.theme.SAFECHILDTheme
-import com.example.safechild.nav.Navigator
+import androidx.activity.viewModels
+import com.example.safechild.viewmodel.PaymentMethodViewModel
+import com.example.safechild.views.nav.Navigator
 
 class MainActivity : ComponentActivity() {
+
+    val viewModel by viewModels<PaymentMethodViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Navigator()
+            Navigator( this,viewModel)
         }
     }
 }
