d6e0eb4fe518dbb69b66f3c3c8a39f99405f70c6
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-16 15:25:53 -0500

    refactor(Payments): add payment method entity

---
+++ app/src/main/java/com/example/safechild/model/beans/PaymentMethod.kt
@@ -0,0 +1,27 @@
+package com.example.safechild.model.beans
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "payment_methods")
+data class PaymentMethod(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int?,
+
+    @ColumnInfo(name = "card_holder_name")
+    var cardHolderName: String,
+
+    @ColumnInfo(name = "card_number")
+    var cardNumber: String,
+
+    @ColumnInfo(name = "expiry_date")
+    var expiryDate: String,
+
+    @ColumnInfo(name = "cvv")
+    var cvv: String,
+
+    @ColumnInfo(name = "card_Type")
+    var cardType: String // VISA, Mastercard, ...
+
+)
