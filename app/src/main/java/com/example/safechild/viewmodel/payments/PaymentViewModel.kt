66bc026c05ac44d9057bf29b311eb442a03190d6
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-05 22:10:53 -0500

    feat: implement payments retrieval by caregiver ID and status

--- app/src/main/java/com/example/safechild/viewmodel/payments/PaymentViewModel.kt
+++ app/src/main/java/com/example/safechild/viewmodel/payments/PaymentViewModel.kt
@@ -2,9 +2,11 @@ package com.example.safechild.viewmodel.payments
 
 import android.content.Context
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.safechild.model.beans.payments.Payment
 import com.example.safechild.model.beans.payments.PaymentMethod
 import com.example.safechild.model.client.RetrofitClient
 import com.example.safechild.model.db.AppDataBase
@@ -13,44 +15,26 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class PaymentViewModel: ViewModel() {
-    var paymentMethod: PaymentMethod = PaymentMethod(1, "", "", "", "", "",)
+    var paymentsList = mutableStateListOf<Payment>()
+        private set
 
-    suspend fun getPaymentMethod(id: Int){
-        val response = RetrofitClient.paymentApiService.getPaymentMethodId(id)
-        if (response.body()!=null){
-            paymentMethod = response.body()!!
+    var payment: Payment= Payment(0 , "", "", 0, 0, 0,0, "")
+
+    suspend fun getPaymentsByCaregiverIdAndPaymentStatus(caregiverId: Long, paymentStatus: String) {
+        try {
+            val response = RetrofitClient.paymentApiService.getPaymentsByCaregiverIdAndPaymentStatus(caregiverId, paymentStatus)
+            if (response.isSuccessful) {
+                response.body()?.let { payments ->
+                    paymentsList.clear()
+                    paymentsList.addAll(payments)
+                }
+            } else {
+                println("HTTP Error: ${response.code()} - ${response.message()}")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
-    var listaPaymentMethod: MutableList<PaymentMethod> by mutableStateOf(arrayListOf())
 
-    fun listarPaymentMethods(context: Context){
-        var appDB: AppDataBase = AppDataBase.Companion.getDatabase(context)
-        GlobalScope.launch {
-
-            listaPaymentMethod=appDB.paymentMethodDao().ListaPaymentMethods().toMutableList()
-        }
-    }
-
-    fun insertPaymentMethod(paymentMethod: PaymentMethod, context: Context){
-        var appDB: AppDataBase = AppDataBase.Companion.getDatabase(context)
-        GlobalScope.launch(Dispatchers.IO) {
-            appDB.paymentMethodDao().insert(paymentMethod)
-        }
-    }
-
-    fun updatePaymentMethod(paymentMethod: PaymentMethod, context: Context){
-        var appDB: AppDataBase = AppDataBase.Companion.getDatabase(context)
-        GlobalScope.launch(Dispatchers.IO) {
-            appDB.paymentMethodDao().update(paymentMethod)
-        }
-    }
-
-    fun deletePaymentMethod(paymentMethod: PaymentMethod, context: Context, onSuccess: () -> Unit = {}){
-        var appDB: AppDataBase = AppDataBase.Companion.getDatabase(context)
-        GlobalScope.launch(Dispatchers.IO) {
-            appDB.paymentMethodDao().delete(paymentMethod)
-            onSuccess()
-        }
-    }
 }
