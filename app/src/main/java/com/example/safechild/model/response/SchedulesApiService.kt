e9a2934cb42a8e32e09472084b19ef08b732166b
P1ero0VL <u20211a620@upc.edu.pe>
2025-07-11 00:47:51 -0500

    feat: update schedules api service

--- app/src/main/java/com/example/safechild/model/response/SchedulesApiService.kt
+++ app/src/main/java/com/example/safechild/model/response/SchedulesApiService.kt
@@ -2,12 +2,29 @@ package com.example.safechild.model.response
 
 import com.example.safechild.model.beans.schedules.Schedule
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface SchedulesApiService {
 
-    @GET("api/v1/caregiver-schedule/{id}")
-    suspend fun getScheduleId(@Path("id") id: Int): Response<List<Schedule>>
+    @GET("api/v1/schedules/{id}")
+    suspend fun getScheduleById(@Path("id") id: Int): Response<List<Schedule>>
+
+
+    @GET("api/v1/schedules/caregiver/{caregiverId}")
+    suspend fun getSchedulesByCaregiverId(@Path("caregiverId") caregiverId: Long): Response<List<Schedule>>
+
+    @POST("api/v1/schedules")
+    suspend fun createSchedule(@Body schedule: Schedule): Response<Schedule>
+
+    @PUT("api/v1/shifts/availability/{id}")
+    suspend fun updateShiftAvailability(@Path("id") id: Long, @Body body: Map<String, Boolean>): Response<Unit>
+
+    @DELETE("api/v1/schedules/{scheduleId}")
+    suspend fun deleteSchedule(@Path("scheduleId") scheduleId: Long): Response<Unit>
 
 }
