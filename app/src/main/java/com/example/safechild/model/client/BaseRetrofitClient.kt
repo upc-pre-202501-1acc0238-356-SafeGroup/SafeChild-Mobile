01870f8892b90d108ce8bbf924245e9f7ad5e546
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-11 18:17:32 -0500

    feat: update Retrofit base URL

--- app/src/main/java/com/example/safechild/model/client/BaseRetrofitClient.kt
+++ app/src/main/java/com/example/safechild/model/client/BaseRetrofitClient.kt
@@ -9,7 +9,7 @@ abstract class BaseRetrofitClient {
 
     companion object {
         //const val BASE_URL = "http://192.168.18.21:8093/"
-        const val BASE_URL = "https://safechild-aeb7cxdtbwgcbcbc.canadacentral-01.azurewebsites.net/"
+        const val BASE_URL = "https://safechild-lastdeploy-a8ahd7ccc8b4cjb3.canadacentral-01.azurewebsites.net/"
 
         val retrofit: Retrofit by lazy {
             val okHttpClient = OkHttpClient.Builder()
